777d8cc90456f7ffe79c2db78d4f18d232a5145b
MohitAlphonsus <mohitdalphonso@gmail.com>
2023-02-02 00:48:36 +0530

    list items styling and rendering, removing functionality added

--- src/components/AddItem.jsx
+++ src/components/AddItem.jsx
@@ -5,11 +5,18 @@ const AddItem = () => {
 
 	const submitHandler = e => {
 		e.preventDefault();
+
+		setEnteredItem(e.target.value);
+		console.log(e.target.value);
+		console.log(enteredItem);
 	};
 
 	return (
-		<form className="flex gap-2">
-			<input type="text" className="border p-3 w-full" />
+		<form className="flex gap-2" onSubmit={submitHandler}>
+			<input
+				type="text"
+				className="border p-3 w-full focus:outline-none focus:border-blue-100"
+			/>
 			<button className="border p-3 basis-1/5 bg-blue-500 text-white uppercase tracking-wider font-medium hover:bg-blue-600">
 				Add
 			</button>
--- src/components/GroceryList.jsx
+++ src/components/GroceryList.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState } from 'react';
 
 const groceryList = [
 	{ id: 1, checked: false, item: 'Bread' },
@@ -7,27 +7,57 @@ const groceryList = [
 ];
 
 const GroceryList = () => {
+	const [groceryItems, setGroceryItems] = useState(groceryList);
+
+	const onCheckHandler = id => {
+		const groceryListItems = groceryItems.map(item =>
+			item.id === id ? { ...item, checked: !item.checked } : item,
+		);
+
+		setGroceryItems(groceryListItems);
+		localStorage.setItem('grocery', JSON.stringify(groceryListItems));
+	};
+
+	const onRemoveItemHandler = id => {
+		const groceryListItems = groceryItems.filter(item => item.id !== id);
+		setGroceryItems(groceryListItems);
+		localStorage.setItem('grocery', JSON.stringify(groceryListItems));
+	};
+
 	return (
-		<ul className="flex flex-col gap-2">
-			{groceryList.map(listItem => (
-				<li
-					key={listItem.id}
-					className="list-none flex items-center justify-between border px-2 py-3 rounded-md"
-				>
-					<div className="flex items-center gap-8">
-						<input
-							type="checkbox"
-							checked={listItem.checked}
-							className=" w-6 h-6"
-						/>
-						<label>{listItem.item}</label>
-					</div>
-					<button className="bg-red-400 py-1 px-2 text-red-50 hover:bg-red-500">
-						remove
-					</button>
-				</li>
-			))}
-		</ul>
+		<div>
+			{groceryItems.length ? (
+				<ul className="flex flex-col gap-2">
+					{groceryItems.map(listItem => (
+						<li
+							key={listItem.id}
+							id={listItem.id}
+							className="list-none flex items-center justify-between border px-2 py-3 rounded-md"
+						>
+							<div className="flex items-center gap-8">
+								<input
+									type="checkbox"
+									checked={listItem.checked}
+									className=" w-6 h-6"
+									onChange={() => onCheckHandler(listItem.id)}
+								/>
+								<label>{listItem.item}</label>
+							</div>
+							<button
+								className="bg-red-400 py-1 px-2 text-red-50 hover:bg-red-500"
+								onClick={() => onRemoveItemHandler(listItem.id)}
+							>
+								remove
+							</button>
+						</li>
+					))}
+				</ul>
+			) : (
+				<p className="text-center text-lg text-slate-400">
+					List is empty, start adding...
+				</p>
+			)}
+		</div>
 	);
 };
 
