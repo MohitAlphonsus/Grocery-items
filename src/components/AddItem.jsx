f3806c85139eae724813f8a447ad4c918256b50a
MohitAlphonsus <mohitdalphonso@gmail.com>
2023-02-02 11:42:53 +0530

    fixed the focus after clicking on add button

--- src/components/AddItem.jsx
+++ src/components/AddItem.jsx
@@ -1,8 +1,13 @@
+import { useRef } from 'react';
+
 const AddItem = ({ enteredItem, setEnteredItem, submitHandler }) => {
+	const inputRef = useRef();
+
 	return (
 		<form className="flex gap-2" onSubmit={submitHandler}>
 			<input
 				autoFocus
+				ref={inputRef}
 				type="text"
 				className="border p-3 w-full focus:outline-none focus:border-blue-100"
 				placeholder="Add Item"
@@ -10,7 +15,10 @@ const AddItem = ({ enteredItem, setEnteredItem, submitHandler }) => {
 				value={enteredItem}
 				onChange={e => setEnteredItem(e.target.value)}
 			/>
-			<button className="border p-3 basis-1/5 bg-blue-500 text-white uppercase tracking-wider font-medium hover:bg-blue-600">
+			<button
+				className="border p-3 basis-1/5 bg-blue-500 text-white uppercase tracking-wider font-medium hover:bg-blue-600"
+				onClick={() => inputRef.current.focus()}
+			>
 				Add
 			</button>
 		</form>
